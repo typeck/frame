d498e907877d9aa21516ceb4b95756a8493190e7
type <tangyp@ushareit.com>
2020-07-29 14:21:38 +0800

    fix flag init

--- flag/cmd.go
+++ flag/cmd.go
@@ -5,7 +5,7 @@ import "fmt"
 func init() {
 	DefaultFlag = New(nil)
 	DefaultFlag.String("conf", "config-test.toml", "config file")
-	DefaultFlag.Bool("conf", false, "watch config change")
+	DefaultFlag.Bool("watch", false, "watch config change")
 	err := DefaultFlag.Parse()
 	if err != nil {
 		panic(err)
